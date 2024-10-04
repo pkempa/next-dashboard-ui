0ab9008edb41cd76211062f5ce02d2d898820d4a
Paul Kempa <paul@monsterlg.com>
2024-10-04 18:15:49 -0400

    Implement Menu and Navbar

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const menuItems = [
   {
     title: "MENU",
@@ -112,3 +115,28 @@ const menuItems = [
     ],
   },
 ];
+
+const Menu = () => {
+  return (
+    <div className="mt-4 text-sm">
+      {menuItems.map((i) => (
+        <div className="flex flex-col gap-2" key={i.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-4">
+            {i.title}
+          </span>
+          {i.items.map((item) => (
+            <Link
+              className="flex items-center justify-center lg:justify-start text-gray-500 gap-4 py-2"
+              key={item.label}
+              href={item.href}
+            >
+              <Image src={item.icon} width={20} height={20} alt="" />
+              <span className="hidden lg:block">{item.label}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+export default Menu;
